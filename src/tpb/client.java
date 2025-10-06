b0c172da31cf419a326453061188390d6d1e0b30
MSI <melek445566778899@gmail.com>
2025-10-06 14:42:05 +0100

    the second commit

--- src/tpb/client.java
+++ src/tpb/client.java
@@ -5,7 +5,8 @@ public class client {
 
 	public static void main(String[] args) throws  IOException {
 		/*1éré étape création de l'objet Soket */
-		Socket socket=new Socket("localhost",3000);
+		Socket socket=new Socket("0.0.0.0",3000);
+		/*le moment ou le client connecte au serveur*/
 		System.out.println("je suis connecté");
 		
 
