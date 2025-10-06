c707353530e4cd3ba60e78eb3912d7cb83e6af51
MSI <melek445566778899@gmail.com>
2025-10-06 14:18:21 +0100

    hello

--- src/tpb/client.java
+++ src/tpb/client.java
@@ -1,9 +1,15 @@
 package tpb;
-
+import java.io.*;
+import java.net.*;
 public class client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws  IOException {
+		/*1éré étape création de l'objet Soket */
+		Socket socket=new Socket("localhost",3000);
+		System.out.println("je suis connecté");
+		/*prendre la main pour éccrire dans le flux de communication*/
+		OutputStream os=socket.getOutputStream();
+		os.write(210);
 
 	}
 
