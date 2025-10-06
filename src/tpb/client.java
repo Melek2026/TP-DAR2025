c4d0b944050728c468e285de1fb9f6f524fab5b4
MSI <melek445566778899@gmail.com>
2025-10-06 14:27:02 +0100

    test

--- src/tpb/client.java
+++ src/tpb/client.java
@@ -7,10 +7,7 @@ public class client {
 		/*1éré étape création de l'objet Soket */
 		Socket socket=new Socket("localhost",3000);
 		System.out.println("je suis connecté");
-		/*prendre la main pour éccrire dans le flux de communication*/
-		OutputStream os=socket.getOutputStream();
-		os.write(210);
-		System.out.println("h");
+		
 
 	}
 
--- src/tpb/servuer.java
+++ src/tpb/servuer.java
@@ -10,14 +10,7 @@ public class servuer {
 		/*2 éme étape :attendre la connexion du client*/
 		Socket socket =Soketserveur.accept();
 		System.out.println("un client est connecté");
-		/*prendre la main pur lire de flux de communication*/
-		InputStream is=socket.getInputStream();
-		int nb=is.read();
 		
-		int x=nb*2;
-		System.out.println(x);
-		OutputStream out=socket.getOutputStream();
-		out.write(x);
 
 	}
 
