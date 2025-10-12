ba1842e6ea75a1153ddbdd2a64ed12c1f080c7d8
MSI <melek445566778899@gmail.com>
2025-10-12 14:49:21 +0100

    my first

--- ACTIVIT2/src/act2/client.java
+++ ACTIVIT2/src/act2/client.java
@@ -6,27 +6,27 @@ import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) throws IOException {
-        // 1. Connexion au serveur
+      
         Socket socket = new Socket("localhost", 3000);
         System.out.println("Je suis connecté au serveur.");
 
-        // 2. Demander un nombre à l'utilisateur
+  
         Scanner input = new Scanner(System.in);
         System.out.print("Entrez un nombre : ");
         int nombre = input.nextInt();
  
-        // 3. Envoyer le nombre au serveur
+        
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        out.writeInt(nombre); // 🔸 envoi de l'entier complet
+        out.writeInt(nombre);
 
-        // 4. Recevoir le résultat du serveur
+  
         DataInputStream in = new DataInputStream(socket.getInputStream());
-        int resultat = in.readInt(); // 🔸 lecture correcte d’un entier
+        int resultat = in.readInt(); 
 
         System.out.println("Résultat reçu du serveur : " + resultat);
         System.out.println("hello");
 
-        // 5. Fermer les flux
+        
         in.close();
         out.close();
         socket.close();
